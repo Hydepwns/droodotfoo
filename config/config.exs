7c0a16e322597b4401c1a5374223812ea356422a
Hydepwns <drew@axol.io>
2025-10-26 01:38:23 +0200

    Update app configuration and modules

--- config/config.exs
+++ config/config.exs
@@ -43,7 +43,7 @@ config :esbuild,
   version: "0.25.4",
   droodotfoo: [
     args:
-      ~w(js/app.ts --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=. --loader:.ts=ts --loader:.tsx=tsx),
+      ~w(js/app.ts --bundle --splitting --format=esm --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=. --loader:.ts=ts --loader:.tsx=tsx),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
   ]
