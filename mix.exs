ec172e387c26c3b91429f64eb050fb27b60cf44b
Hydepwns <drew@axol.io>
2026-01-25 17:47:27 +0100

    chore: bump version to 1.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Droodotfoo.MixProject do
   def project do
     [
       app: :droodotfoo,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
